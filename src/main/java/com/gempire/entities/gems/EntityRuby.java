1108cc23903c80c44f5b9828f68eee6b0c199751
Family <segapop4@gmail.com>
2023-05-01 09:10:50 -0500

    Visor Rework

--- src/main/java/com/gempire/entities/gems/EntityRuby.java
+++ src/main/java/com/gempire/entities/gems/EntityRuby.java
@@ -143,7 +143,11 @@ public class EntityRuby extends EntityGem {
         return false;
     }
 
-    public int generateOutfitVariant(){
+    public int generateOutfitVariant() {
+        if (getGemPlacement() == 11)
+            return 11;
+        else if (getGemPlacement() == 17)
+            return 17;
         return this.random.nextInt(7);
     }
 
